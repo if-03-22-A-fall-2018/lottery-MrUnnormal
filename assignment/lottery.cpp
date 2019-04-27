689844e9c3220b01caa7f2e3546c4c45a028cb22
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-04-27 10:26:02 +0200

    2 => final

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -17,37 +17,107 @@
 #define 	UUID_LEN   40
 #define 	MAX_TIP_LEN   17
 #define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN + 1)
-char seperator;
+#define   MAX_TIPS 1000000
+
+int drawing[TIP_SIZE];
+char separator;
 FILE* fd;
 
 bool 	init_lottery (const char *csv_file, char csv_separator)
 {
   FILE* file = fopen(csv_file, "r");
-  seperator = csv_separator;
+  separator = csv_separator;
   fd = file;
+  for (int i = 0; i < TIP_SIZE; i++)
+  { drawing[i] = 0; }
   return fd != 0;
 }
 
-bool 	get_tip (int tip_number, int tip[TIP_SIZE])
+int search_start(char* line)
 {
-  fseek(fd, tip_number + UUID_LEN, SEEK_SET);
-  for (size_t i = 0; i < count; i++)
+  for (int i = 0; i < MAX_LINE_LEN; i++)
   {
-    /* code */
+    if(line[i]==separator)
+    { return i+1; }
   }
+  return -1;
+}
+
+bool get_tip(int tip_number, int tip[TIP_SIZE])
+{
+  char line[MAX_LINE_LEN];
+  fseek(fd, 0 , SEEK_SET);
+  if(tip_number<0)
+  { return false; }
+  for (int i = 0; i <= tip_number; i++)
+  {
+    if(fgets(line, MAX_LINE_LEN, fd) == 0)
+    { return false; }
+  }
+  int index=0;
+  int count=0;
+  for (int i = search_start(line); i < MAX_LINE_LEN; i++)
+  {
+    char numbers[2];
+    if(line[i]==separator || line[i]=='\n' || line[i]=='\0')
+    {
+      int num=0;
+      for (int i = 0; i < count; i++)
+      { num=num*10+numbers[i]-'0'; }
+      tip[index++]=num;
+      count=0;
+      if(index==TIP_SIZE)
+      { break; }
+    }
+    else
+    { numbers[count++]=line[i]; }
+  }
+  return true;
 }
 
 bool 	set_drawing (int drawing_numbers[TIP_SIZE])
 {
-  return false;
+  for(int i = 0; i < TIP_SIZE; i++)
+  {
+    if(drawing_numbers[i] < 1 || drawing_numbers[i] > 45)
+    { return false; }
+    drawing[i] = drawing_numbers[i];
+  }
+  return true;
 }
 
 int 	get_tip_result (int tip_number)
 {
-  return 0;
+  if(drawing[0] == 0)
+  { return -1; }
+  int tips[TIP_SIZE];
+  if(!get_tip(tip_number, tips))
+  { return -2; }
+  int count = 0;
+  for (int i = 0; i < TIP_SIZE; i++)
+  {
+    for (int o = 0; o < TIP_SIZE; o++)
+    {
+      if(tips[i] == drawing[o])
+      { count++; }
+    }
+  }
+  return count;
 }
 
 int 	get_right_tips_count (int right_digits_count)
 {
-  return 0;
+  if(right_digits_count < 0 || right_digits_count > TIP_SIZE || drawing[0] == 0)
+  { return -1; }
+  int count=0;
+  fseek(fd, 0, SEEK_SET);
+  char line[MAX_LINE_LEN];
+  for (int i = 0; i < MAX_TIPS; i++)
+  {
+    if(fgets(line, MAX_LINE_LEN, fd)==0)
+    { return count; }
+    if(get_tip_result(i)==right_digits_count)
+    { count++; }
+  }
+  return count;
 }
